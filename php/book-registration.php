dc80c532dc3cc6ed04e9543c26574f3511b123ca
ImmeD <i_aivazashvili@cu.edu.ge>
2014-03-24 20:22:43 +0400

    modified server response

--- php/book-registration.php
+++ php/book-registration.php
@@ -6,11 +6,11 @@ function getContent ($array) {
 	$author = $array['book-author'];
 	$arr["error"] = array("message" => "Book Was Not Registered");
 	if (!empty($name) && !empty($author)) {
-		http_response_code(201);
+		header("HTTP/1.1 201 Created");
 		header('Content-type: application/json');
 		return (getJson($_POST));
 	} else {
-		http_response_code(400);
+		header("HTTP/1.1 400 Bad Request");
 		header('Content-type: application/json');
 		return(getJson($arr));
 	}
